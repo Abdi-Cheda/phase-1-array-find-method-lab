e5768fd2ba4bce31e177bdfb89e9c9d728a9b026
+ <abdicheda75@gmail.com>
2023-10-08 08:18:59 +0300

    Assignment Done!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W"); // Find a game where the result is a win ("W")
+    if (win) {
+        return win.year; // If a win is found, return the year
+    } else {
+        return undefined; // If no win is found, return undefined
+    }
+}
+const record = [
+    { year: 1998, result: "W" },
+    { year: 2005, result: "L" },
+    { year: 2013, result: "W" },
+    // ... more game objects ...
+];
+
+const winningYear = superbowlWin(record);
+console.log(winningYear); // Output: 1998 (or undefined if there are no wins in the record)
+
